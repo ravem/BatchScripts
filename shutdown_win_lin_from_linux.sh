f12294b5ac29a406d626c9b2bb3858927655db3d
ravem <transalp98@gmail.com>
2019-01-21 19:24:26 +0100

    Update shutdown_win_lin_from_linux.sh

--- shutdown_win_lin_from_linux.sh
+++ shutdown_win_lin_from_linux.sh
@@ -28,9 +28,8 @@ echo "Controllo i computer linux accesi e lancio lo shutdown" |tee -a log.txt
 IFS=","
 while read ip name
 do
-			 ping -c2 -W1 "$ip" && sshpass -p PASSWORD ssh -o StrictHostKeyChecking=no -t -t USERNAME@"$ip" ' sudo poweroff' >/dev/null   && echo "Ho lanciato lo shutdown di "$name"" 
-		
-			done < linux.txt
+	ping -c2 -W1 "$ip" && sshpass -p PASSWORD ssh -o StrictHostKeyChecking=no -t -t USERNAME@"$ip" ' sudo poweroff' >/dev/null   && echo "Ho lanciato lo shutdown di "$name"" 
+	done < linux.txt
 		
 echo"" |tee -a log.txt
 
@@ -39,9 +38,8 @@ echo "Controllo i computer windows accesi e lancio lo shutdown" |tee -a log.txt
 IFS=","
 while read ip name
 do
-			 ping -c2-W1 "$ip" && net rpc shutdown -I "$ip" -f -t 1 -U USERNAME%PASSWORD |& tee -a log.txt && echo "Ho lanciato lo shutdown di "$name"" | tee -a log.txt
-			
-			done < windows.txt
+	ping -c2-W1 "$ip" && net rpc shutdown -I "$ip" -f -t 1 -U USERNAME%PASSWORD |& tee -a log.txt && echo "Ho lanciato lo shutdown di "$name"" | tee -a log.txt
+	done < windows.txt
 
 jumpto mid
 
@@ -61,13 +59,13 @@ echo "" |tee -a log.txt
 clear
 
 echo "Controllo se qualcosa è ancora acceso" |tee -a log.txt
-		while read ip name; do 
-			ping -c2 -W1 $ip >/dev/null && echo ""$name" è ancora acceso, devo ricontrollarlo" |& tee -a log.txt || echo ""$name" è spento" |& tee -a log.txt
-		done < linux.txt
+	while read ip name; do 
+	ping -c2 -W1 $ip >/dev/null && echo ""$name" è ancora acceso, devo ricontrollarlo" |& tee -a log.txt || echo ""$name" è spento" |& tee -a log.txt
+	done < linux.txt
 
-		while read ip name; do
-			ping -c2 -W1 $ip >/dev/null && echo "$name è ancora acceso, devo ricontrollarlo" |& tee -a log.txt || echo "$name è spento" |& tee -a log.txt
-		done < win.txt
+	while read ip name; do
+	ping -c2 -W1 $ip >/dev/null && echo "$name è ancora acceso, devo ricontrollarlo" |& tee -a log.txt || echo "$name è spento" |& tee -a log.txt
+	done < win.txt
 
 
 #Metto un timer di 10 secondi per essere ragionevolmente sicuro che tutto si spenga
@@ -85,14 +83,14 @@ echo "Secondo e ultimo controllo" |tee -a log.txt
 IFS=","
 while read ip name
 do
-			ping -c2 -W1 $ip >/dev/null && echo -e "\e[0;31m$name è ancora acceso, verifica!\e[0m" && echo "$name è ancora acceso" >> log.txt || echo -e "\e[0;32m$name è spento\e[0m" && echo "$name è spento" >> log.txt
-		done < linux.txt
+	ping -c2 -W1 $ip >/dev/null && echo -e "\e[0;31m$name è ancora acceso, verifica!\e[0m" && echo "$name è ancora acceso" >> log.txt || echo -e "\e[0;32m$name è spento\e[0m" && echo "$name è spento" >> log.txt
+	done < linux.txt
 
 IFS=","
 while read ip name
 do
-			ping -c2 -W1 $ip >/dev/null && echo -e "\e[0;31m$name è ancora acceso, verifica!\e[0m" && echo "$name è rimasto acceso" >> log.txt 
-		done < windows.txt
+	ping -c2 -W1 $ip >/dev/null && echo -e "\e[0;31m$name è ancora acceso, verifica!\e[0m" && echo "$name è rimasto acceso" >> log.txt 
+	done < windows.txt
 
 #Chiudo il log
 
